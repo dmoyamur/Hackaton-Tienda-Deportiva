8f18b93c67f51871036b12494c17f0f4a041d32f
yudithleon <yudithkleon@gmail.com>
2025-11-24 11:29:14 -0500

    uniendo y js de filtrar

---
+++ scripts/filtrarCategorias.js
@@ -0,0 +1,11 @@
+import { mostrarProductos } from "./mostrarProductos.js";
+
+export const filtrarCategoria = (productos, categoria) => {
+  if (categoria === "Todas") {
+    mostrarProductos(productos);
+    return;
+  }
+
+  const filtrados = productos.filter((p) => p.categoria === categoria);
+  mostrarProductos(filtrados);
+};
