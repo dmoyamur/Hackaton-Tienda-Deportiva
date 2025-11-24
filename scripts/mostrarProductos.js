ac8114d527f05751607928596e1cbc9784e8c1f5
santiago-r21 <santiagorioscajamarca@gmail.com>
2025-11-24 15:55:30 -0500

    se arreglan animaciones

--- scripts/mostrarProductos.js
+++ scripts/mostrarProductos.js
@@ -6,7 +6,9 @@ export const mostrarProductos = (lista) => {
     contenedor.innerHTML += `
       <div class="col-md-4 mb-4">
         <div class="card" style="width: 20rem; height: 40rem">
-          <img src="${prod.imagen}" class="card-img-top" alt="${prod.nombre}">
+        <div class= cont_imagen>
+          <img src="${prod.imagen}" class="card-img-top img-producto" alt="${prod.nombre}">
+          </div>
           <div class="card-body">
             <h5 class="card-title">${prod.nombre}</h5>
             <p class="card-text">${prod.descripcion}</p>
