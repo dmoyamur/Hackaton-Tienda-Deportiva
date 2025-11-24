ac010398c2dc349f9ace89aea009d46b240d2136
yudithleon <yudithkleon@gmail.com>
2025-11-24 14:38:26 -0500

    arreglando el total y subtotal del carrito

--- scripts/agregarCarrito.js
+++ scripts/agregarCarrito.js
@@ -1,9 +1,17 @@
 export let carrito = JSON.parse(localStorage.getItem("carritoCompra")) || [];
 
 export const agregarAlCarrito = (producto) => {
-  console.log("ingrese al ca", producto)
-  carrito?.push(producto);
-  console.log("carrito", carrito)
+  console.log("ingrese al ca", producto);
+  console.log("carrito", carrito);
+
+  const existe = carrito.find((item) => item.id === producto.id);
+
+  if (existe) {
+    existe.cantidad += 1;
+  } else {
+    carrito.push({ ...producto, cantidad: 1 });
+  }
+
   localStorage.setItem("carritoCompra", JSON.stringify(carrito));
 
   Swal.fire({
@@ -17,32 +25,48 @@ export const agregarAlCarrito = (producto) => {
 
 export const mostrarCarrito = () => {
   const contenedor = document.getElementById("carrito-body");
+  const totalContenedor = document.getElementById("carrito-total");
+
+  let total = 0;
+
   contenedor.innerHTML = "";
 
   if (carrito.length === 0) {
     contenedor.innerHTML = "<p>El carrito está vacío.</p>";
+    if (totalContenedor) totalContenedor.textContent = "";
     return;
   }
 
+  
   carrito.forEach((item) => {
+    const subtotal = item.precio * item.cantidad;
+    total += subtotal;
+
     contenedor.innerHTML += `
       <div class="d-flex justify-content-between align-items-center border-bottom py-2">
-      <div class="d-flex align-items-center gap-2">
-        <img 
-          src="${item.imagen}" 
-          style="width: 50px; height: 50px; object-fit: cover; border-radius: 5px;"
-        >
-        <div>
-        <div>
-          <strong>${item.nombre}</strong><br>
-          <small>$${item.precio}</small>
+
+        <div class="d-flex align-items-center gap-2">
+          <img 
+            src="${item.imagen}" 
+            style="width: 50px; height: 50px; object-fit: cover; border-radius: 5px;"
+          >
+          <div>
+            <strong>${item.nombre}</strong><br>
+            <small>$${item.precio} x ${item.cantidad}</small><br>
+            <strong>Subtotal: $${subtotal}</strong>
+          </div>
         </div>
+
         <button class="btn btn-danger btn-sm eliminar-item" data-id="${item.id}">
           Eliminar
         </button>
       </div>
     `;
+      
+   
   });
+ totalContenedor.textContent = `Total a pagar: $${total}`;
+  
 };
 
 export const eliminarDelCarrito = (id) => {
