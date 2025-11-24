ab6ed1f20379dea22babe285a4f34dc4cedd4a56
yudithleon <yudithkleon@gmail.com>
2025-11-24 13:07:56 -0500

    carrito casi listo

--- scripts/agregarCarrito.js
+++ scripts/agregarCarrito.js
@@ -1,8 +1,18 @@
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+export let carrito = JSON.parse(localStorage.getItem("carritoCompra")) || [];
 
 export const agregarAlCarrito = (producto) => {
-  carrito.push(producto);
-  localStorage.setItem("carrito", JSON.stringify(carrito));
+  console.log("ingrese al ca", producto)
+  carrito?.push(producto);
+  console.log("carrito", carrito)
+  localStorage.setItem("carritoCompra", JSON.stringify(carrito));
+
+  Swal.fire({
+    title: "Producto agregado",
+    text: `${producto.nombre} fue agregado al carrito`,
+    icon: "success",
+    timer: 1500,
+    showConfirmButton: false,
+  });
 };
 
 export const mostrarCarrito = () => {
@@ -17,17 +27,24 @@ export const mostrarCarrito = () => {
   carrito.forEach((item) => {
     contenedor.innerHTML += `
       <div class="d-flex justify-content-between align-items-center border-bottom py-2">
+      <div class="d-flex align-items-center gap-2">
+        <img 
+          src="${item.imagen}" 
+          style="width: 50px; height: 50px; object-fit: cover; border-radius: 5px;"
+        >
+        <div>
         <div>
           <strong>${item.nombre}</strong><br>
           <small>$${item.precio}</small>
         </div>
-        <button class="btn btn-danger btn-sm eliminar-item" data-id="${item.id}">Eliminar</button>
+        <button class="btn btn-danger btn-sm eliminar-item" data-id="${item.id}">
+          Eliminar
+        </button>
       </div>
     `;
   });
 };
 
-// Eliminar producto del carrito
 export const eliminarDelCarrito = (id) => {
   carrito = carrito.filter((item) => item.id != id);
   localStorage.setItem("carrito", JSON.stringify(carrito));
