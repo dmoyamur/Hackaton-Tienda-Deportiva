58b22d076744524eaa00a2e41e212ffe3eecc3c6
dmoyamur <dmoya844@gmail.com>
2025-11-24 12:15:58 -0500

    Cambios de diseño

--- scripts/mostrarProductos.js
+++ scripts/mostrarProductos.js
@@ -5,13 +5,13 @@ export const mostrarProductos = (lista) => {
   lista.map((prod) => {
     contenedor.innerHTML += `
       <div class="col-md-4 mb-4">
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 20rem; height: 40rem">
           <img src="${prod.imagen}" class="card-img-top" alt="${prod.nombre}">
           <div class="card-body">
             <h5 class="card-title">${prod.nombre}</h5>
             <p class="card-text">${prod.descripcion}</p>
             <p class="card-text">${prod.categoria}</p>
-            <p class="card-text">$ ${prod.precio}</p>
+            <p class="card-text precio">$ ${prod.precio}</p>
              <!-- Botón para agregar al carrito -->
             <button 
               class="btn btn-primary agregar-carrito" 
